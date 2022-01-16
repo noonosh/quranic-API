5fd9d15bdc74be4f974c6d19c608837f49cc08fb
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-01-16 20:51:27 +0000

    learning how to read from file

--- utils/readFromFile.js
+++ utils/readFromFile.js
@@ -1,4 +1,27 @@
 import fs from 'fs';
-import path from 'path';
 
-fs.readFile('');
+function jsonReader(filePath, cb) {
+	fs.readFile(filePath, (err, fileData) => {
+		if (err) {
+			return cb && cb(err);
+		}
+		try {
+			const object = JSON.parse(fileData);
+			return cb && cb(null, object);
+		} catch (err) {
+			return cb && cb(err);
+		}
+	});
+}
+
+// const readFromFile = pathToFile => {
+// 	fs.readFile(pathToFile, (err, jsonString) => {
+// 		if (err) {
+// 			console.error('File read failed:', err);
+// 			return;
+// 		}
+// 		return JSON.parse(jsonString);
+// 	});
+// };
+
+export default jsonReader;
