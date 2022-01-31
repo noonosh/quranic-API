66d7fc9bba40a3895ebee3016a9c1f6c152310ae
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-01-31 23:29:01 +0000

    Use helmet in the app

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ import express from 'express';
 import surahRoutes from './v1/routes/surah.js';
 import reciterRoutes from './v1/routes/reciter.js';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(helmet());
+
 app.use('/api/v1/surah', surahRoutes);
-app.use('/api/v1/reciter', reciterRoutes);
+app.use('/api/v1/reciters', reciterRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
